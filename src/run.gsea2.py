9a8148b7a5af51c010f717a51000c7f0b89b031b
Anthony S. Castanza <acastanza@ucsd.edu>
2021-07-21 14:31:43 -0700

    Update run script

--- src/run.gsea2.py
+++ src/run.gsea2.py
@@ -17,6 +17,7 @@ def main():
 	ap.add_argument("-m","--gmt",action="store",dest="GMT",help="GMT file.")
 	options = ap.parse_args()
 
+	os.mkdir("gsea_results")
 
 	sc_ge_sa = gp.data.GCT(options.GCT) # Parse GCT file
 	cls_file = gp.data.CLS(options.CLS) # Parse CLS file
@@ -27,21 +28,21 @@ def main():
 	sa_la.index = sc_ge_sa.columns # Assign sample names to classes
 	
 	nu_se_st = run_gsea(
-	    sc_ge_sa, # Gene-by-sample score; DataFrame
-	    sa_la, # Sample label; Series
-	    gm, # Gene sets; set-to-genes dict or DataFrame
-	    no="-0-", # Normalization method; "-0-", "0-1", "1234", "log"
-	    ra="si", # Ranking method; "ic", "si", "co", "tt", "di", "ra", "lo"
-	    mi=5, # Minimum gene set size; int
-	    ma=500, # Maximum gene set size; int
-	    we=1.0, # Weight used for "ks" and "auc"; float
-	    me="ks", # Enrichment method; "ks", "auc", "js"
-	    se=1729, # Random seed; int
-	    pe="label", # Permutation type; "label", ""
-	    n_pe=1000, # Number of permutations; int
-	    n_pl=25, # Number of extreme gene sets to plot; int
-	    ad=None, # Additional gene sets to plot; list of str
-	    pa="~/", # directory path to write the gene-set-by-statistic and plots; str
+        sc_ge_sa,  # Gene-by-sample score; DataFrame
+        sa_la,  # Sample label; Series
+        gm,  # Gene sets; set-to-genes dict or DataFrame
+        no="-0-",  # Normalization method; "-0-", "0-1", "1234", "log"
+        ra="ic",  # Ranking method; "ic", "si", "co", "tt", "di", "ra", "lo"
+        mi=5,  # Minimum gene set size; int
+        ma=500,  # Maximum gene set size; int
+        we=1.0,  # Weight used for "ks" and "auc"; float
+        me="js",  # Enrichment method; "ks", "auc", "js"
+        se=1729,  # Random seed; int
+        pe="label",  # Permutation type; "gene_set", "label"
+        n_pe=1000,  # Number of permutations; int
+        n_pl=25,  # Number of extreme gene sets to plot; int
+        ad=None,  # Additional gene sets to plot; list of str
+        pa="gsea_results", # directory path to write the gene-set-by-statistic and plots; str
 	)
 	
 	nu_se_st

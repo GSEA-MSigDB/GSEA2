b98401c03641f8abf99995be2e0140dd3c42babb
Anthony S. Castanza <acastanza@ucsd.edu>
2021-07-21 15:25:59 -0700

    Update julia install

--- src/install.jl
+++ src/install.jl
@@ -1,11 +1,13 @@
 using Pkg: add
 
+add(url="https://github.com/KwatME/Kwat.jl")
+
 for na in [
     "PyCall",
+    "DataFrames",
+    "Pandas",
 ]
 
     add(na)
 
 end
-
-add(url="https://github.com/KwatME/Kwat.jl")

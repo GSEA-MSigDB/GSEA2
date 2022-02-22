fccf7cd894b03c2ec84066ca3784eaf29012bd37
Anthony S. Castanza <acastanza@ucsd.edu>
2022-02-21 17:04:26 -0800

    Add module files

---
+++ module/GSEAlib.py
@@ -0,0 +1,153 @@
+import os, sys, pandas, numpy
+
+# Simple GMT/GMX to Dict parser
+def read_sets(gene_sets_dbfile_list):
+    genesets={}
+    genesets_descr={}
+    for gsdb in gene_sets_dbfile_list:
+        gsdb_split = gsdb.split(".")
+        if gsdb_split[-1] == "gmt":
+            with open(gsdb) as f:
+                temp=f.read().splitlines()
+                for i in range(len(temp)):
+                    gs_line=temp[i].split("\t")
+                    gene_set_name=gs_line[0]
+                    gene_set_desc=gs_line[1]
+                    gene_set_tags=gs_line[2:len(gs_line)]
+                    genesets[gene_set_name]=list(set(gene_set_tags))
+                    genesets_descr[gene_set_name]=gene_set_desc # Not used yet but should end up in reports eventually
+        else:  # is a gmx formatted file
+            df_temp=pandas.read_csv(
+                gsdb, sep='\t', skip_blank_lines=True).transpose().dropna(how='all')
+            for i in range(len(df_temp)):
+                gs_line=df_temp.iloc[i][~df_temp.iloc[i].isnull()]
+                gene_set_name=gs_line.name
+                gene_set_desc=gs_line[0]
+                gene_set_tags=gs_line[1:len(gs_line)]
+                genesets[gene_set_name]=list(set(gene_set_tags))
+                genesets_descr[gene_set_name]=gene_set_desc # Not used yet but should end up in reports eventually
+    return genesets
+
+
+# Simple implementation of a GCT parser
+# Accepts a GCT file and returns a Pandas Dataframe with a single index
+def read_gct(gct):
+    dataset=pandas.read_csv(gct, sep='\t', header=2, index_col=[
+        0, 1], skip_blank_lines=True)
+    dataset.index.names=["NAME", "Description"]
+    dataset_descriptions=dataset.index.to_frame(index=False)
+    dataset_descriptions.set_index(["NAME"], inplace=True)
+    dataset.index=dataset.index.droplevel(1)  # Drop gene descriptions
+    return {'data': dataset, 'row_descriptions': dataset_descriptions["Description"].values}
+
+
+# Simple implementation of a CHIP Parser for use with ssGSEA
+# Reads in a CHIP formatted file and returns a pandas dataframe containing
+# the probe to gene mappings
+def read_chip(chip):
+    chip_df=pandas.read_csv(chip, sep='\t', index_col=0, skip_blank_lines=True)
+    return chip_df
+
+
+# Simple implementation of GSEA DEsktop's Collapse Dataset functions for use
+# with ssSGEA
+# Accepts an expression dataset in GCT format, a CHIP file, and a
+# collapse metric and returns a pandas dataframe formatted version of the
+# dataset collapsed from probe level to gene level using the specified metric.
+def collapse_dataset(dataset, chip, method="sum"):
+    import pandas as pd
+    if isinstance(dataset, pandas.DataFrame):
+        dataset=dataset
+    elif isinstance(dataset, dict) == False:
+        dataset=read_gct(dataset)
+    if isinstance(chip, pandas.DataFrame) == False:
+        chip=read_chip(chip)
+    if isinstance(dataset, dict) == True:
+        dataset=dataset['data']
+    joined_df=chip.join(dataset, how='inner')
+    joined_df.reset_index(drop=True, inplace=True)
+    annotations=joined_df[["Gene Symbol",
+                             "Gene Title"]].drop_duplicates().copy()
+    joined_df.drop("Gene Title", axis=1, inplace=True)
+    if method.lower() == "sum":
+        collapsed_df=joined_df.groupby(["Gene Symbol"]).sum()
+    if method.lower() == "mean":
+        collapsed_df=joined_df.groupby(["Gene Symbol"]).mean()
+    if method.lower() == "median":
+        collapsed_df=joined_df.groupby(["Gene Symbol"]).median()
+    if method.lower() == "max":
+        collapsed_df=joined_df.groupby(["Gene Symbol"]).max()
+    if method.lower() == "absmax":
+        collapsed_df = joined_df.loc[joined_df.groupby(['Gene Symbol'].idxmax()]
+    collapsed_df.index.name="Name"
+    return {'data': collapsed_df, 'row_descriptions': annotations["Gene Title"].values}
+
+
+# Save a GCT result to a file, ensuring the filename has the extension .gct
+def write_gct(gct, file_name, check_file_extension=True):
+    if check_file_extension:
+        file_name = check_extension(file_name, ".gct")
+
+    rows = str(len(gct['data']))
+    columns = str(len(gct['data'].columns))
+
+    if len(gct['row_descriptions'])!=int(rows):
+        sys.exit("Number of row descriptions ("+ len(gct['row_descriptions'])+ ") not equal to number of row names ("+ rows+ ").")
+
+    row_descriptions = gct['row_descriptions']
+    if row_descriptions == None:
+        row_descriptions = ['NA']*int(rows)
+
+    m = gct['data'].copy()
+    m.insert(loc=0, column='Description', value=gct['row_descriptions'])
+    with open(file_name, 'w') as file:
+        file.write('#1.2\n' + rows + '\t' + columns + '\n')
+        m.to_csv(file, sep='\t', index_label="NAME", mode='w+')
+    return(file_name)
+
+
+# extension e.g. '.gct'
+def check_extension(file_name, extension):
+    import re
+    ext = re.search(extension+"$", file_name.lower())
+    if ext == None:
+        file_name = file_name + extension
+    return file_name
+
+
+# Read CLS function adapted from https://github.com/broadinstitute/gsea_python/blob/ccal-refactor/gsea/Utils.py
+def read_cls(path):
+    """
+    Returns a Pandas Series with phenotype labels from a .cls
+        path (str): Local filepath for a .cls file
+        returns (pd.Series): Pandas Series with phenotype labels
+    """
+    lines = open(path).readlines()
+    labels = {
+        label: i for i, label in enumerate(lines[1][1:-1].split())
+    }
+    try:
+        labs = lines[2][:-1].split()
+        phens = [labels[lab] for lab in lines[2].strip('\n').split()]
+        return pandas.concat([pandas.Series(labs, name='Labels'), pandas.Series(phens, name='Phenotypes')], axis=1)
+    except KeyError: ## Assume phenotype row is already ints
+        phens = list(map(int, lines[2].strip('\n').split()))
+        return pandas.concat([pandas.Series(phens, name='Labels'), pandas.Series(phens, name='Phenotypes')], axis=1)
+
+
+# Read Maps Phenotypes to Samples, adapted from https://github.com/broadinstitute/gsea_python/blob/ccal-refactor/gsea/Utils.py
+def match_phenotypes(expr, phen):
+    """
+    Populates the index of phen with the column names of expr
+        expr (pandas.DataFrame): DataFrame from read_gct
+        phen (pandas.Series): Series from read_cls
+        returns (pandas.Series): phen with index set to expr columns
+    """
+    if len(phen) == len(expr.columns):
+        phen.index = expr.columns
+    else:
+#        common = set(phen['phenotypes'].index) & set(expr.columns)
+#        expr = expr[list(common)]
+#        phen = phen[list(common)]
+        sys.exit("The number of samples in the CLS file did not match the number of samples in the dataset.")
+    return phen
--- module/run.gsea2.py
+++ module/run.gsea2.py
@@ -1,65 +1,104 @@
-limport os, sys
+import os, sys, subprocess
 from optparse import OptionParser
 import argparse
 import shutil
-import pickle
+import json
 
-import gp
-import kwat
-from gp import data
-from gsea import run_gsea
-
-import pandas as pd
-import numpy as np
+import pandas
+import numpy
 
 def main():
 	usage="%prog [options]" + "\n"
 	ap = argparse.ArgumentParser()
-	ap.add_argument("-g","--gct",action="store",dest="GCT",help="GCT file.")
-	ap.add_argument("-c","--cls",action="store",dest="CLS",help="CLS file.")
-	ap.add_argument("-m","--gmt",action="store",dest="GMT",help="GMT file.")
-	ap.add_argument("-r","--rme",action="store",dest="rme",help="Ranking Metric.")
-	ap.add_argument("-e","--eme",action="store",dest="eme",help="Enrichment Method.")
-	ap.add_argument("-p","--perm",action="store",dest="perm",default="label",help="Permutation mode.")
-	ap.add_argument("-n","--nperm",action="store",dest="nperm",default=1000,type=int,help="Number of permutations.")
-	ap.add_argument("-a","--max",action="store",dest="max",default=500,type=int,help="Max gene set size.")
-	ap.add_argument("-i","--min",action="store",dest="min",default=5,type=int,help="Min gene set size.")
-	ap.add_argument("-w","--wgt",action="store",dest="weight",default=1.0,type=float,help="Weight for ks or auc enrichment method.")
-	ap.add_argument("-l","--npl",action="store",dest="nplot",default=25,type=int,help="Number of enrichment results to plot.")
-	ap.add_argument("-z","--rnd",action="store",dest="rnd",default=1729,type=int,help="Random seed used for permutations.")
-	ap.add_argument("-j","--cpu",action="store",dest="cpu",default=1,type=int,help="Job CPU Count.")
+	ap.add_argument("--libdir", action="store",
+					dest="libdir", help="Working directory to load support library from.")
+	ap.add_argument("--dataset",action="store",dest="dataset",help="Input Expression Dataset.")
+	ap.add_argument("--gsdb",action="store",dest="gsdb",help="Gene Set Database File.")
+	ap.add_argument("--nperm",action="store",dest="nperm",default=1000,type=int,help="Number of permutations.")
+	ap.add_argument("--cls",action="store",dest="cls",help="CLS file.")
+	ap.add_argument("--reverse",action="store",dest="reverse",default="False",help="Reverse the phenotype comparison defined in the CLS file.")
+	ap.add_argument("--permute",action="store",dest="perm",default="False",help="Reverse the phenotype comparison defined in the CLS file.")
+	ap.add_argument("--perm",action="store",dest="perm",default="sample",help="Permutation mode. Options are 'sample' (phenotype) and 'set' (gene set).")
+	ap.add_argument("--collapse",action="store",dest="collapse",default="none",help="Method for computing mathematical collapse. Supports 'none' ('no collapse'), 'sum', 'mean', 'median', 'max', 'absmax'")
+	ap.add_argument("--chip",action="store",dest="chip",default="none",help="Chip file used for performing collapse.")
+	ap.add_argument("--metric",action="store",dest="rank_metric",help="Metric for ranking genes.")
+	ap.add_argument("--method",action="store",dest="method",help="Enrichment Method. 'ks' (old GSEA) and 'js' (next gen GSEA) supported.")
+	ap.add_argument("--weight",action="store",dest="weight",default=1.0,type=float,help="Weight for ks or auc enrichment method.")
+	ap.add_argument("--max",action="store",dest="max",default=500,type=int,help="Max gene set size.")
+	ap.add_argument("--min",action="store",dest="min",default=15,type=int,help="Min gene set size.")
+	ap.add_argument("--seed",action="store",dest="seed",default=1729,type=int,help="Random seed used for permutations.")
+	ap.add_argument("--nplot",action="store",dest="nplot",default=25,type=int,help="Number of enrichment results to plot.")
+	ap.add_argument("--cpu",action="store",dest="cpu",default=1,type=int,help="Job CPU Count.")
 	options = ap.parse_args()
 
+	sys.path.insert(1, options.libdir)
+	import GSEAlib
+
 	os.mkdir("gsea_results")
+	os.mkdir("gsea_results/input_files")
 
-	sc_el_sa = gp.data.GCT(options.GCT) # Parse GCT file
-	cls_file = gp.data.CLS(options.CLS) # Parse CLS file
-	se_el_ = kwat.gmt.read([options.GMT]) # Parse GMT file
-	
-	sc_el_sa.index=sc_el_sa.index.droplevel(1) # Drop gene descriptions
-	ta = pd.Series(cls_file.class_assignments) # extract class assignments from CLS object
-	ta = np.asarray(ta) # convert to numpy array
-	#ta.index = sc_el_sa.columns # Assign sample names to classes ## No longer works with numpy array class
-	
-	nu_se_st = run_gsea(
-        ta,  # Sample label; Series
-        sc_el_sa,  # Gene-by-sample score; DataFrame
-        se_el_,  # Gene sets; set-to-genes dict or DataFrame
-        fu=options.rme,  # Ranking method; "ic", "si", "co", "tt", "di", "ra", "lo"
-        mi=options.min,  # Minimum gene set size; int
-        ma=options.max,  # Maximum gene set size; int
-        n_jo=options.cpu,
-        we=options.weight,  # Weight used for "ks" and "auc"; float
-        al=options.eme,  # Enrichment method; "ks", "auc", "js"
-        pe=options.perm,  # Permutation type; "gene_set", "label"
-        ra=options.rnd,  # Random seed; int
-        n_pe=options.nperm,  # Number of permutations; int
-        n_pl=options.nplot,  # Number of extreme gene sets to plot; int
-        ad=None,  # Additional gene sets to plot; list of str
-        pa="gsea_results", # directory path to write the gene-set-by-statistic and plots; str
-	)
 
-	pickle.dump(nu_se_st,open("gsea_results/complete_result.pkl", "wb"))
+	## Parse GMT/GMX files from a list of inputs and create a name:members dict written out as a json file
+	if options.gene_sets_db_list_filename != None:
+		with open(options.gene_sets_db_list_filename) as f:
+			gene_sets_dbfile_list = f.read().splitlines()
+
+	genesets=GSEAlib.read_sets(gene_sets_dbfile_list)
+	with open('gsea_results/input_files/set_to_genes.json', 'w') as path:
+		json.dump(genesets, path,  indent=2)
+
+
+	## Parse GCT file
+	if options.input_gct_filename.split(".")[-1] == "gct":
+		if options.collapse != "none":
+			chip_file=read_chip(options.chip)
+			input_ds = GSEAlib.collapse_dataset(options.input_gct_filename, chip_file, method=options.collapse)
+		else:
+			input_ds = GSEAlib.read_gct(options.input_gct_filename)
+		input_ds=input_ds['data']
+	else:
+		input_ds=pandas.read_csv(options.input_gct_filename, sep='\t', index_col=0, skip_blank_lines=True)
+		if "description" in input_ds.columns.str.lower():
+			description_loc=input_ds.columns.str.lower().to_list().index('description')
+			input_ds.drop(input_ds.columns[[description_loc]], axis = 1, inplace = True)
+			input_ds.index.name="Name"
+		if options.collapse != "none":
+			chip_file=read_chip(options.chip)
+			input_ds = GSEAlib.collapse_dataset(input_ds, chip_file, method=options.collapse)
+			input_ds=input_ds['data']
+
+	## Parse CLS file
+	phenotypes=GSEAlib.read_cls(options.cls)
+	phenotypes=GSEAlib.match_phenotypes(input_ds,phenotypes)
+	if(options.reverse=="True"):
+		phenotypes["Phenotypes"]=numpy.where((phenotypes["Phenotypes"]==0)|(phenotypes["Phenotypes"]==1), phenotypes["Phenotypes"]^1, phenotypes["Phenotypes"])
+	phenotypes=phenotypes.sort_values('Phenotypes')
+
+	## Order the dataset using the phenotypes and write out both files
+	input_ds=input_ds.reindex(columns=phenotypes.index)
+	input_ds.to_csv('gsea_results/input_files/gene_by_sample.tsv', sep = "\t")
+	pandas.DataFrame(phenotypes['Phenotypes']).transpose().to_csv('gsea_results/input_files/target_by_sample.tsv',sep="\t", index=False)
+
+	## Construct GSEA Settings json file
+	gsea_settings={
+		"number_of_permutations": options.nperm,
+		"permutation": options.perm,
+		"metric": options.rank_metric,
+		"algorithm": options.method,
+		"weight": options.weight,
+		"maximum_gene_set_size": options.max,
+		"minimum_gene_set_size": options.min,
+		"random_seed": options.seed,
+		"number_of_jobs": options.cpu,
+		"number_of_extreme_gene_sets_to_plot": options.nplot,
+		"gene_sets_to_plot": []
+	}
+
+	with open('gsea_results/input_files/gsea_settings.json', 'w') as path:
+		json.dump(gsea_settings, path,  indent=2)
+
+	## Run GSEA
+	subprocess.check_output(['gsea', 'standard', 'gsea_results/input_files/gsea_settings.json', 'gsea_results/input_files/set_to_genes.json', 'gsea_results/input_files/target_by_sample.tsv', 'gsea_results/input_files/gene_by_sample.tsv', 'gsea_results'])
 
 if __name__ == '__main__':
 	main()
